7fe0cf497f4f6fee57883a2d17e235ac6ae451e5
Timur Dzhalalov <timurdzhalalov@yahoo.com>
2021-08-23 15:29:23 +0300

    Add project files.

---
+++ ShortestPathProblem/ShortestPathProblem.cpp
@@ -0,0 +1,13 @@
+﻿// ShortestPathProblem.cpp: определяет точку входа для приложения.
+//
+
+#include "ShortestPathProblem.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Inital commit" << endl;
+
+	return 0;
+}
